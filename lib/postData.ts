c556eeadd99b9e1cc221327f87bbf2c23b218db9
Cat4848 <catalinrol@gmail.com>
2023-12-04 15:57:42 +0000

    feat:add-new-author-sql-function-created

--- lib/postData.ts
+++ lib/postData.ts
@@ -1,6 +1,18 @@
-import { sql } from "@vercel/postgres";
-import { IUser, IAuthor, IBook } from "./definitions";
+import { sql } from '@vercel/postgres';
+import { IUser, IAuthor, IBook } from './definitions';
+import type { TemplateString } from 'next/dist/lib/metadata/types/metadata-types';
 
-export async function postAuthor() {
-  
+export async function postAuthor(author: IAuthor) {
+  const { first_name, last_name } = author;
+
+  try {
+    const newAuthor = sql`INSERT INTO authors (first_name last_name)
+      VALUES (
+        ${first_name}, ${last_name}
+        );
+      `;
+    console.log('newAuthor result: ', newAuthor);
+  } catch (error) {
+    console.error('Add new author error: ', error);
+  }
 }
