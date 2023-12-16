0c2fc7c2e91468deaa6746d7090a8523b89e2548
Cat4848 <catalinrol@gmail.com>
2023-12-16 18:09:28 +0000

    feat:reander-authors-fetch-authors-hook-added

--- components/authors/RenderAuthors.tsx
+++ components/authors/RenderAuthors.tsx
@@ -1,7 +1,10 @@
+import styled from 'styled-components';
+import useFetchAuthors from '../../lib/useFetchAuthors';
+
 interface IRenderAuthors {
   url: string;
 }
 
 export default function RenderAuthors({ url }: IRenderAuthors) {
-  
+  const { authors } = useFetchAuthors(url);
 }
