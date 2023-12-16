793961de66afbc57e869ddd35f5a0f837cec2ddf
Cat4848 <catalinrol@gmail.com>
2023-12-16 17:47:39 +0000

    feat:search-author-initial-setup

--- pages/authors/index.tsx
+++ pages/authors/index.tsx
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+
+let apiUrl = '/api/authors';
+
 export default function Authors() {
+  const [searchAuthor, setSearchAuthor] = useState('');
+
+  if (searchAuthor !== '') {
+    apiUrl = apiUrl + `?author=${searchAuthor}`;
+  }
+
   return <h1>Authors Page</h1>;
 }
