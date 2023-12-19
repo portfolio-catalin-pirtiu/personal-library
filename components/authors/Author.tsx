424f051b2ba53b2da04cefb48e3b0fdd304b40c8
Cat4848 <catalinrol@gmail.com>
2023-12-19 16:01:41 +0000

    feat:edit-author-ititial-setul

--- components/authors/Author.tsx
+++ components/authors/Author.tsx
@@ -2,11 +2,15 @@ import styled from 'styled-components';
 import { IAuthor } from '../../lib/definitions';
 import { Heading1 } from '../../lib/text';
 
+const Wrapper = styled.div``;
+
 const FirstAndLastName = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 4vmin;
 `;
+
+const EditAndDelete = styled.div``;
 
 const FirstName = styled(Heading1)``;
 
@@ -14,9 +18,13 @@ const LastName = styled(Heading1)``;
 
 export default function Author({ first_name, last_name }: IAuthor) {
   return (
-    <FirstAndLastName>
-      <FirstName>{first_name}</FirstName>
-      <LastName>{last_name}</LastName>
-    </FirstAndLastName>
+    <Wrapper>
+      <FirstAndLastName>
+        <FirstName>{first_name}</FirstName>
+        <LastName>{last_name}</LastName>
+      </FirstAndLastName>
+
+      <EditAndDelete></EditAndDelete>
+    </Wrapper>
   );
 }
