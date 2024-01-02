6afd6286d3b571736aed72b79d93108976988623
Cat4848 <catalinrol@gmail.com>
2024-01-02 21:23:16 +0000

    chore:book-class-changed

--- pages/books/new.tsx
+++ pages/books/new.tsx
@@ -1,3 +1,32 @@
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FormikValues,
+  FormikErrors,
+} from 'formik';
+import styled from 'styled-components';
+import toast from 'react-hot-toast';
+import { IBook } from '../../lib/definitions';
+
+const StyledForm = styled(Form)``;
+
+const Label = styled.label``;
+
+const Input = styled(Field)``;
+
+const ErrorMsg = styled(ErrorMessage)`
+  color: red;
+`;
+
+const InputGroup = styled.div``;
+
+const SubmitButton = styled.button<{ disabled: boolean }>`
+  cursor: ${({ disabled }) => (disabled ? 'wait' : 'pointer')};
+  opacity: ${({ disabled }) => disabled && '0.5'};
+`;
+
 export default function NewBook() {
   return <h1>New Book Page</h1>;
 }
