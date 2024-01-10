76c7f93cd05116e1d9211fbb44e893d74e6e2af0
Cat4848 <catalinrol@gmail.com>
2024-01-10 18:18:18 +0000

    feat:Book-component-initial-outline

--- components/books/Book.tsx
+++ components/books/Book.tsx
@@ -1,3 +1,45 @@
-export default function Book({}){
+import { IBookWithAuthor } from '../../lib/definitions';
+import styled from 'styled-components';
 
+const Wrapper = styled.div``;
+const RestOfInfo = styled.div``;
+const AuthorTitleAndStatus = styled.div``;
+const Edition = styled.div``;
+const Publisher = styled.div``;
+const Notes = styled.div``;
+const StartStopButtons = styled.div``;
+const Title = styled.div``;
+const Author = styled.div``;
+
+export default function Book({
+  id,
+  author_id,
+  title,
+  first_name,
+  last_name,
+  edition,
+  publisher,
+  notes,
+  in_progress,
+  read,
+  rating,
+  start_reading,
+  stop_reading,
+}: IBookWithAuthor) {
+  return (
+    <Wrapper>
+      <RestOfInfo>
+        <Edition></Edition>
+        <Publisher></Publisher>
+        <Notes></Notes>
+      </RestOfInfo>
+
+      <StartStopButtons></StartStopButtons>
+      
+      <AuthorTitleAndStatus>
+        <Author></Author>
+        <Title></Title>
+      </AuthorTitleAndStatus>
+    </Wrapper>
+  );
 }
