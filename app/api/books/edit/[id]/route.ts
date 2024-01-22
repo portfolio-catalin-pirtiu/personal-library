068474e5a688063bb4ef46544987756f858f1976
Cat4848 <catalinrol@gmail.com>
2024-01-22 12:26:02 +0000

    feat:api-route-PUT-book-implemented

--- app/api/books/edit/[id]/route.ts
+++ app/api/books/edit/[id]/route.ts
@@ -1,8 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { putBook } from '../../../../../lib/putData';
+import { IBook } from '../../../../../lib/definitions';
 
-export default function PUT(
+export default async function PUT(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const id = params.id;
+  const book: IBook = await req.json();
+
+  try {
+    await putBook(book, id);
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.log('edit book DB Error', error);
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { name: error.name, message: error.message },
+        { status: 500 }
+      );
+    }
+  }
 }
