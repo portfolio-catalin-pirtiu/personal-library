4525df00bcf095526fa064a702e88581e36c8f8f
Cat4848 <catalinrol@gmail.com>
2024-01-30 14:22:07 +0000

    feat/added-author-details-to-initial-values

--- components/books/Book.tsx
+++ components/books/Book.tsx
@@ -1,4 +1,4 @@
-import { IDbBook, IBookWithAuthor } from '../../lib/definitions';
+import { IBookWithAuthor } from '../../lib/definitions';
 import styled from 'styled-components';
 import Button from '../shared/Button';
 import { colors } from '../../lib/colors';
@@ -48,7 +48,7 @@ export default function Book({
   stop_reading,
 }: IBookWithAuthor) {
   const [isEditing, setIsEditing] = useState(false);
-  const editBookInitialValues: IDbBook = {
+  const editBookInitialValues: IBookWithAuthor = {
     book_id: book_id,
     author_id: author_id,
     title: title,
@@ -58,6 +58,8 @@ export default function Book({
     publisher: publisher,
     edition: edition,
     notes: notes,
+    first_name: first_name,
+    last_name: last_name,
   };
   function handleStartReading() {}
   function handleStopReading() {}
