bcf58e68c89b10da2adc08847e4d78472eb63941
Cat4848 <catalinrol@gmail.com>
2024-01-31 07:22:30 +0000

    feat:Bubble-component-initial-setup

---
+++ components/shared/Bubble.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+
+interface IBubble{
+  text: string;
+  color: string;
+}
+
+export default function Bubble({}) {
+  return (
+    <Wrapper></Wrapper>
+  )
+}
