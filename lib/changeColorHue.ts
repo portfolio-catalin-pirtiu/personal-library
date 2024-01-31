14f5e690f9537030e5c3f8fe9356e55ecacec403
Cat4848 <catalinrol@gmail.com>
2024-01-31 11:22:24 +0000

    feat:change-color-hue-completed

--- lib/changeColorHue.ts
+++ lib/changeColorHue.ts
@@ -16,13 +16,14 @@ export function changeColorHue({
   const currentHue = Number(
     `${original[index - 2]}` + `${original[index - 1]}`
   );
-  let darkerHue: string = '';
-  if (lighter) darkerHue = String(currentHue + points);
-  if (darker) darkerHue = String(currentHue - points);
+  
+  let newHue = String(currentHue - points);
+  if (lighter) newHue = String(currentHue + points);
+  if (lighter && darker || !lighter && !darker) newHue;
 
   const newHueArray = Array.from(original, (el, i) => {
-    if (i === index - 2) return darkerHue[0];
-    if (i === index - 1) return darkerHue[1];
+    if (i === index - 2) return newHue[0];
+    if (i === index - 1) return newHue[1];
     return el;
   });
   return newHueArray.join('');
