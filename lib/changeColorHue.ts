0e4a817764ff54901ed16d775b4b40d08a54f101
Cat4848 <catalinrol@gmail.com>
2024-01-31 11:04:11 +0000

    feat:added-change-colour-logic

--- lib/changeColorHue.ts
+++ lib/changeColorHue.ts
@@ -1,3 +1,26 @@
-export function changeColorHue(original: string){
-  
+import { colors } from './colors';
+
+interface ChangeColorHue {
+  original: string;
+  lighter?: boolean;
+  darker?: boolean;
+  points: number;
+}
+export function changeColorHue({
+  original = colors.gray,
+  lighter = false,
+  darker = true,
+  points = 30,
+}: ChangeColorHue) {
+  const index = original.lastIndexOf('%');
+  const currentHue = Number(
+    `${original[index - 2]}` + `${original[index - 1]}`
+  );
+  const darkerHue = String(currentHue - points);
+
+  const newHueArray = Array.from(original, (el, i) => {
+    if (i === index - 2) return darkerHue[0];
+    if (i === index - 1) return darkerHue[1];
+    return el;
+  });
 }
