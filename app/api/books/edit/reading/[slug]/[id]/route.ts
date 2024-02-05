a8686662698d0de0d665cf5650c5abb7cab336d5
Cat4848 <catalinrol@gmail.com>
2024-02-05 18:14:57 +0000

    refactor:removed-console-logs

--- app/api/books/edit/reading/[slug]/[id]/route.ts
+++ app/api/books/edit/reading/[slug]/[id]/route.ts
@@ -8,8 +8,5 @@ export async function POST(
   req: NextRequest,
   { params }: { params: { id: string; slug: 'start' | 'stop' } }
 ) {
-  console.log('POST start reading');
   const book: BookReadingStatus = await req.json();
-  console.log('params', params);
-  console.log('start stop book reading -> body', book.timestamp);
 }
