df18f1ad1b28c6aaab6889a53f3aa4bb7a4ce030
Cat4848 <catalinrol@gmail.com>
2024-02-05 12:11:00 +0000

    feat:update-book-reading-status-database-function-outline-completed

--- lib/updateBookReadingStatusDatabase.ts
+++ lib/updateBookReadingStatusDatabase.ts
@@ -1,5 +1,24 @@
+import { booksApiUrl } from './constants';
+import toast from 'react-hot-toast';
+
 export default async function updateBookReadingStatusDatabase(
   id: string = '',
   status: 'start' | 'stop' = 'start',
-  url: string = ''
-) {}
+  timestamp: Date = new Date()
+) {
+  const url = `${booksApiUrl}/edit/reading/${status}/${id}`;
+  const readingStatus = {
+    [status]: timestamp,
+  };
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(readingStatus),
+    });
+  } catch (error) {
+    if (error instanceof Error) toast.error('Something went wrong');
+  }
+}
