9f8d09aa8c8450f4faeb0e269386aca1f055948d
Cat4848 <catalinrol@gmail.com>
2024-03-14 14:22:26 +0000

    add: image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+import libraryImage from '../assets/open-book-in-library.jpeg';
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>;
+      <Image
+        src={libraryImage}
+        alt="open-book-in-a-library"
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+      />
+    </>
+  );
 }
