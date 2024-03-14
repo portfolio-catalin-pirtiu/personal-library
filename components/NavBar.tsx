e27e36e2b9a1f8e33f148447f98c17a67e85098e
Cat4848 <catalinrol@gmail.com>
2024-03-14 16:57:14 +0000

    add: navbar rearrange elements

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,22 +4,27 @@ import LinkWrapper from './LinkWrapper';
 import { color } from '../lib/color';
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
   background-color: ${color.black};
   color: ${color.silver};
   padding: 1vmin 1 vmin;
 `;
 
-const Links = styled.ul`
+const LinksContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: calc(1vw + 2lh);
-  justify-items: center;
-  justify-content: space-between;
+  // justify-items: center;
+  justify-content: flex-end;
   margin: 0;
   padding: 2vmin;
   padding-bottom: 0.2lh;
   font-size: calc(0.7rem + 1vw);
 `;
+
+const LogoContainer = styled.div``;
 
 interface INavBarItem {
   href: string;
@@ -39,14 +44,17 @@ export default function NavBar({
 
   return (
     <Wrapper>
-      <Links>
+      <LogoContainer>
         <LinkWrapper href="/">{logo}</LinkWrapper>
+      </LogoContainer>
+
+      <LinksContainer>
         {navbarItems.map((item) => (
           <LinkWrapper href={`/${item.href}`} key={item.href}>
             {item.linkName}
           </LinkWrapper>
         ))}
-      </Links>
+      </LinksContainer>
     </Wrapper>
   );
 }
