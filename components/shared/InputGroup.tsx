5c849f738cc955e949fe31fd7da2690e37acea58
Cat4848 <catalinrol@gmail.com>
2024-03-21 21:08:23 +0000

    add: InputGroup component

---
+++ components/shared/InputGroup.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { Field, ErrorMessage } from 'formik';
+import { color } from '../../lib/color';
+
+const InputAndLabel = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`;
+
+const Label = styled.label`
+  margin-bottom: 0.3em;
+`;
+
+const Input = styled(Field)`
+  margin-bottom: 0.5em;
+  border-radius: 7px;
+  border: 0.15em solid ${color.silver};
+  padding: 0.5em;
+`;
+
+const ErrorMsg = styled(ErrorMessage)`
+  color: red;
+`;
+
+interface IInputGroup {
+  label: string;
+  name: string;
+  autoComplete: string;
+}
+
+export default function InputGroup({
+  label = '',
+  name = '',
+  autoComplete = '',
+}: IInputGroup) {
+  return (
+    <InputAndLabel>
+      <Label htmlFor={name}>{label}</Label>
+      <Input type="input" name={name} autoComplete={autoComplete} />
+      <ErrorMsg name="name" component="div" />
+    </InputAndLabel>
+  );
+}
