e8240ed73eb5d2af60596845aa39045e737b1e17
Cat4848 <catalinrol@gmail.com>
2024-05-31 15:07:29 +0100

    test: testing coverage for the Label component

--- components/shared/Label/Label.tsx
+++ components/shared/Label/Label.tsx
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export default function Label({}) {}
+const LabelElement = styled.label``;
+
+interface LabelProps {
+  label: string;
+  htmlFor?: string;
+}
+
+export default function Label({
+  label = 'Enter label name',
+  htmlFor,
+}: LabelProps) {
+  return <LabelElement htmlFor={htmlFor}>{label}</LabelElement>;
+}
