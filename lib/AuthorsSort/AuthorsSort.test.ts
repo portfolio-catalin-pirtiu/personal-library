9377cd02ebce9d18eed8e9e9f654b814449eea52
Cat4848 <catalinrol@gmail.com>
2024-06-05 16:27:06 +0300

    test: created tests for the isCurrentAuthorBeforeTheNext method

--- lib/AuthorsSort/AuthorsSort.test.ts
+++ lib/AuthorsSort/AuthorsSort.test.ts
@@ -7,6 +7,24 @@ const authors: IDbAuthor[] = [
   { author_id: '22', first_name: 'Clock', last_name: 'Still' },
 ];
 
+test('if current author is not before the next', () => {
+  const currAuthor = 'Black';
+  const nextAuthor = 'Alpha';
+  const authorsSorter = new AuthorsSorter();
+  expect(
+    authorsSorter.isCurrentAuthorBeforeTheNext(currAuthor, nextAuthor)
+  ).toBe(false);
+});
+
+test('if current author is before the next', () => {
+  const currAuthor = 'Alpha';
+  const nextAuthor = 'Black';
+  const authorsSorter = new AuthorsSorter();
+  expect(
+    authorsSorter.isCurrentAuthorBeforeTheNext(currAuthor, nextAuthor)
+  ).toBe(true);
+});
+
 test('if the authors are sorted ascending on first name', () => {
   const authorsSorter = new AuthorsSorter();
   authors.sort(authorsSorter.ascending);
